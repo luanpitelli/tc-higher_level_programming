6917b77bd6cc22c346c9eac5a68385835b969460
luanpitelli <luanpitelligomes@.com>
2025-12-02 22:15:42 +0000

    6. Concatenar strings - Correção final de sintaxe e conteúdodo

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Holberton School!"
-print(f"{str1}{str2}")
+str1 = "escola"
+str2 = "Holberton!"
+str10 = str1 + str2
+print(f"Bem vinda a {str10}")
