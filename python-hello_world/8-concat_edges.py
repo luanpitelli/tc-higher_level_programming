609da5830380ad3e5bce066273af300bb4b1927c
luanpitelli <luanpitelligomes@.com>
2025-12-02 22:21:49 +0000

    6. Concatenar strings - Correção final de sintaxe e conteúdo

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\nlanguage that combines remarkable power with very clear syntax"
+print(str[39:66])
+print(str[:6])
+print(str[-6:])
