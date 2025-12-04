9511dcd7512194f53d3769a92608a0a41f1d9b25
luanpitelli <luanpitelligomes@gmail.com>
2025-12-04 05:14:19 +0000

    Fix 6-concat.py to match checker output

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = " Holberton School!"
-str1 = str1 + str2
-print(str1)
+str1 = "Battery"
+str2 = "Street"
+print("Welcome to {} {}!".format(str1, str2))
