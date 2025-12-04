d48158f7c333446f4f0f96758bbaca42894b7781
luanpitelli <luanpitelligomes@gmail.com>
2025-12-04 05:11:49 +0000

    Complete 6-concat.py exercise

--- python-hello_world/6-concat_update.py
+++ python-hello_world/6-concat_update.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = " Battery Street!"
-str1 = str1 + str2
-print(str1)
+str1 = "Holberton"
+str2 = "School"
+print("Welcome to {} {}!".format(str1, str2))
