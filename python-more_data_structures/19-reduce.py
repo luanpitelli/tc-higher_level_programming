a0dec722440504bbbb8f3c9969bfb7e80b22fc62
luanpitelli <luanpitelligomes@gmail.com>
2025-12-18 04:51:28 +0000

    Fix: Correção do erro de índice na tarefa 18

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from functools import reduce
 def calc_average(a_dictionary):
-    if not a_dictionary:
+    if not a_dictionary or len(a_dictionary) == 0:
         return 0, 0
-    total_age = reduce(lambda x, y: x + y['age'], a_dictionary, 0)
-    total_salary = reduce(lambda x, y: x + y['salary'], a_dictionary, 0)
+    # Somamos os valores extraindo-os de cada dicionário 'y'
+    total_age = reduce(lambda x, y: x + y.get('age', 0), a_dictionary, 0)
+    total_salary = reduce(lambda x, y: x + y.get('salary', 0), a_dictionary, 0)
     count = len(a_dictionary)
     return total_salary / count, total_age / count
